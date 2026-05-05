3bed0d5e7f27205dd36395905bc691bca3604cb2
Ikundabayo Placide <ikundabayoplacide500@gmail.com>
2026-05-05 15:40:55 +0200

    added remote db

--- src/config/db.js
+++ src/config/db.js
@@ -3,15 +3,15 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const sequelize=new Sequelize(process.env.DATABASE_URL, {
-        dialect: "postgres",
-        logging: false,
-        dialectOptions: {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false
-            }
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: "postgres",
+    logging: false,
+    dialectOptions:{
+        ssl:{
+        require:true,
+        rejectUnauthorized:false
         }
-    })
+    }
+})
 
 export default sequelize
